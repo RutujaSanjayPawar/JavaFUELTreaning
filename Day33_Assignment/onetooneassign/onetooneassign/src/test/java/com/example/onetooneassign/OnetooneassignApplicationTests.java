e68a779e83c65dba0cdbe53e1c4fcdf8a09a7ab5
RutujaSanjayPawar <105098770+RutujaSanjayPawar@users.noreply.github.com>
2022-05-16 10:28:50 +0530

    initial Commit

---
+++ Day33_Assignment/onetooneassign/onetooneassign/src/test/java/com/example/onetooneassign/OnetooneassignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onetooneassign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetooneassignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
