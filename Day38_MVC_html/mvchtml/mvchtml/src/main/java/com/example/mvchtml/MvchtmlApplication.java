00d017f2ff304e9f622bc197b070477124446126
RutujaSanjayPawar <105098770+RutujaSanjayPawar@users.noreply.github.com>
2022-05-20 12:47:40 +0530

    initial Commit

---
+++ Day38_MVC_html/mvchtml/mvchtml/src/main/java/com/example/mvchtml/MvchtmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvchtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvchtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvchtmlApplication.class, args);
+	}
+
+}
---
+++ Day38_MVC_html/mvchtml/mvchtml/src/test/java/com/example/mvchtml/MvchtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvchtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvchtmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
