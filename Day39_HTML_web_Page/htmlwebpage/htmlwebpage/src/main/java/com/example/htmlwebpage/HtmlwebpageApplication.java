5e3e1ccf58e90afe55d3da16946a9e6b1be0422c
RutujaSanjayPawar <105098770+RutujaSanjayPawar@users.noreply.github.com>
2022-05-21 13:08:27 +0530

    initial Commit

---
+++ Day39_HTML_web_Page/htmlwebpage/htmlwebpage/src/main/java/com/example/htmlwebpage/HtmlwebpageApplication.java
@@ -0,0 +1,13 @@
+package com.example.htmlwebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlwebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlwebpageApplication.class, args);
+	}
+
+}
