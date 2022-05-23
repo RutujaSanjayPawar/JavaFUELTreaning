4cb93094d37d98456eedb3a5ade6929eb3cfb695
RutujaSanjayPawar <105098770+RutujaSanjayPawar@users.noreply.github.com>
2022-05-23 13:11:55 +0530

    Initial Commit

---
+++ Day40_HTML_Selection/htmlselectionwebpage/htmlselectionwebpage/src/main/java/com/example/htmlselectionwebpage/HtmlselectionwebpageApplication.java
@@ -0,0 +1,13 @@
+package com.example.htmlselectionwebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlselectionwebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlselectionwebpageApplication.class, args);
+	}
+
+}
